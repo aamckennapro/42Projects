cdc02fbd580bf5ce92dc2b0abf61997c7ba7f19c
aamckennapro <am286517@ohio.edu>
2021-02-15 12:33:16 -0500

    Added webserver.go, mostly just comments

--- 01. Web Server/webserver.go
+++ 01. Web Server/webserver.go
@@ -6,4 +6,25 @@ Last Update: 02/15/21
 Description: A lightweight webserver chock-full of features that works
 primarly via the command prompt. Those features are explained in README.md.
 */
+
 package main
+
+//import () // Going to need to import things eventually, just here for later
+
+// Going to need a whole bunch of functions, probably a class/struct or two, and I foresee the use of channels as well.
+// Likely going to start by implementing HTML/1.0, and will slowly work on bringing the server up to date with 1.1 and 2.0
+
+/* STRUCTS */
+
+// HttpRequest
+// Socket
+// Server
+// Command -- for the cmd prompt
+
+/* FUNCS */
+
+/* MAIN */
+
+// Note: Will only return index.html, 404.html, and 501.html.
+// 404 is the "this page doesn't exist" page, and
+// 501 is the "this page is currently under construction" page
